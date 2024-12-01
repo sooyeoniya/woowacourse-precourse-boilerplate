9afabeef65cdcd9921c1e639f3c7fb5f9af17316
sooyeoniya <tndus1306403@naver.com>
2024-12-01 18:46:13 +0900

    feat(Controller): 입력값에 대한 유효성 검증 및 반환 로직 구현

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,8 +1,29 @@
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
+import validatedSomething from '../validations/validateSomething.js';
 
 class Controller {
 
+  async start() {
+    const something = await this.#getValidatedSomething();
+
+  }
+
+  /**
+   * 입력 값에 대하여 유효성 검증 후 반환하는 로직 (다시 입력 받는 로직 포함)
+   * @returns {string | number | Array<number> | Array<string>} 파싱된 입력 값
+   */
+  async #getValidatedSomething() {
+    try {
+      const something = await InputView.read();
+      // 입력 값에 대한 파싱이 필요한 경우: const parsedSomething = parser.xxx(something);
+      validatedSomething(something);
+      return something;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.#getValidatedSomething();
+    }
+  }
 }
 
 export default Controller;
