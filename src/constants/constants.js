32942cbd2955dde4a5e9b645807ed50300c67d5d
sooyeoniya <tndus1306403@naver.com>
2024-12-01 18:28:09 +0900

    style: 들여쓰기 오류 수정
    - 들여쓰기 4 -> 2 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ import deepFreeze from '../utils/deepFreeze.js';
 
 export const INPUT_MESSAGES = Object.freeze({
   DEFAULT: '\n~~를 입력해 주세요.\n',
-	CALLBACK: (param) => `\n${param}을 입력해 주세요.\n`,
+  CALLBACK: (param) => `\n${param}을 입력해 주세요.\n`,
 });
 
 export const OUTPUT_MESSAGES = Object.freeze({
